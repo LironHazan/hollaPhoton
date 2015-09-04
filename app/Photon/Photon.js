1c77589aaf28b711330d87f83e39c11f3734765f
LironHazan <liron@gigaspaces.com>
2015-09-04 16:00:18 +0300

    added angular side

---
+++ app/Photon/Photon.js
@@ -0,0 +1,25 @@
+/**
+ * Created by liron on 9/4/15.
+ */
+'use strict';
+
+angular.module('Photon', [
+    'ngAnimate',
+    'toastr',
+    'ngCookies',
+    'ngResource',
+    'ui.router',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
+]);
+
+
+angular.module('Photon').config(function($stateProvider) {
+
+    $stateProvider.state('Photon1', {
+        url: '/home',
+        templateUrl: 'Photon/Photon.html',
+        controller: 'PhotonCtrl'
+    });
+});
