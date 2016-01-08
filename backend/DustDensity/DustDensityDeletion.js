df6bd19d2e87effd8abdb8be6a4f557a1f4b7da1
LironHazan <liron@gigaspaces.com>
2016-01-08 11:47:34 +0200

    removed redundant class

--- backend/DustDensity/DustDensityDeletion.js
+++ backend/DustDensity/DustDensityDeletion.js
@@ -4,6 +4,7 @@
 
 var CronJob = require('cron').CronJob;
 
+//todo: continue
 new CronJob('* * 3 * * 0-6', function() { //run everyday once in 3 hours
     console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
